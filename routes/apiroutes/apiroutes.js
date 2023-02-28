ecbceca761368d56319b57d8760f2d7aa954cf37
unknown <danielgregoire34@gmail.com>
2023-02-27 20:25:22 -0500

    adding path file and creating api and htmlroutes to replace app. get function that would call thoses htmls inside the server js

---
+++ routes/apiroutes/apiroutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const notesdb = require('../../db/db.json');
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = 3001;
+const dbjson = require('./db/db.json');
+
+const htmlRoute=require('./routes/htmlroutes');
+const apiRoute = require('./routes/apiroutes/apiroutes');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
-app.get('/send', (req, res) =>
+/*
+app.get('/', (req, res) =>
 res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
+*/
+app.get('/api',(req,res)=> res.json(dbjson))
+
+app.use('/api',apiRoute);
+app.use('/',htmlRoute);
+
 
 app.listen(PORT, () =>
 console.log(`Example app listening at http://localhost:${PORT}`)
