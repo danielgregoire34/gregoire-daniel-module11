64cbc2715036d90f4218706eecec953d43a2ef5d
unknown <danielgregoire34@gmail.com>
2023-03-06 23:29:57 -0500

    added mainly to apiroutes and fixing the post and get route, also fixing a mistake in htmlroutes

--- routes/apiroutes/apiroutes.js
+++ routes/apiroutes/apiroutes.js
@@ -1,6 +1,28 @@
 const router = require('express').Router();
+const fs = require('fs');
+const db = require('../../db/db.json');
 
-const notesdb = require('../../db/db.json');
+router.get('/notes',(req,res)=>{
+res.json(db);
+})
+
+router.post('/notes',(req,res)=>{
+
+let writeNote = db.length + 1;
+
+req.body.writeNote = writeNote++;
+
+db.push(req.body);
+
+fs.writeFile('./db/db.json', JSON.stringify(db), function (err) {
+if (err) throw err;
+
+res.json(db);
+});
+
+})
+
+
 
 
 
--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3001;
-const dbjson = require('./db/db.json');
 
 const htmlRoute=require('./routes/htmlroutes');
 const apiRoute = require('./routes/apiroutes/apiroutes');
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
-
 /*
 app.get('/', (req, res) =>
 res.sendFile(path.join(__dirname, 'public/index.html'))
@@ -20,7 +19,11 @@ app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 */
-app.get('/api',(req,res)=> res.json(dbjson))
+
+
+app.get('/tutor', (req,res) => {
+    res.json('testing 123')
+});
 
 app.use('/api',apiRoute);
 app.use('/',htmlRoute);
